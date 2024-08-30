ff406218caf4e3c79c8783f63177c059b48da75d
saifil <saifilmomin4@gmail.com>
2024-08-29 23:04:04 -0400

    google maps basic setup

---
+++ components/map.tsx
@@ -0,0 +1,43 @@
+/* 
+Since the map was loaded on client side, 
+we need to make this component client rendered as well else error occurs
+*/
+"use client";
+
+//Map component Component from library
+import { GoogleMap } from "@react-google-maps/api";
+
+//Map's styling
+export const defaultMapContainerStyle = {
+  width: "100%",
+  height: "100vh",
+};
+
+const defaultMapCenter = {  
+  lat: 43.706956,
+  lng: -79.345726,
+};
+
+const defaultMapZoom = 18;
+
+const defaultMapOptions = {
+  zoomControl: true,
+  tilt: 0,
+  gestureHandling: "auto",
+  mapTypeId: "satellite",
+};
+
+const MapComponent = () => {
+  return (
+    <div className="w-full">
+      <GoogleMap
+        mapContainerStyle={defaultMapContainerStyle}
+        center={defaultMapCenter}
+        zoom={defaultMapZoom}
+        options={defaultMapOptions}
+      ></GoogleMap>
+    </div>
+  );
+};
+
+export { MapComponent };
